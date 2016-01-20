ca57733be78498e985e5b56cfe582e35f894bb33
alanik <alanik@github.com>
2016-01-20 02:10:50 +0100

    added partial support for vertical navigation

--- dzieuo.js
+++ dzieuo.js
@@ -9,19 +9,19 @@
 		/////////////////////////////////////////
 		//private objects
 		/////////////////////////////////////////
-		var ViewPortItem = function ( columns, column, row )
+		var ViewPortItem = function ( data, column, row )
 		{
 			this.column = column;
 			this.row = row;
 
 			this.get$Column = function ()
 			{
-				return columns[this.column].$column;
+				return data.structure.columns[this.column].$column;
 			}
 
 			this.get$Row = function ()
 			{
-				return columns[this.column].rows[this.row];
+				return data.structure.columns[data.viewPort.currentItem.column].rows[this.row];
 			}
 		}
 
@@ -35,7 +35,7 @@
 				$horizontalPaging: null,
 				$prevHorizontalArrow: null,
 				$nextHorizontalArrow: null,
-				columns: [ //{ $column : $(column), rows : [ $(row) , $(row) ], numOfRows = rows.length }
+				columns: [ //{ $column : $(column), rows : [ $(row) , $(row) ], numOfRows = rows.length, currentRow = 0 }
 				],
 				numOfColumns: 0
 			},
@@ -64,7 +64,7 @@
 				{
 					numOfColumns++;
 					var $column = $( this );
-					var column = { "$column": $column, "rows": [] };
+					var column = { "$column": $column, "rows": [], "currentRow": 0 };
 
 					structure.columns.push( column );
 
@@ -82,11 +82,11 @@
 				structure.numOfColumns = numOfColumns;
 			},
 			// 2
-			setUpViewPortItems: function ( viewPort, columns )
+			setUpViewPortItems: function ( data )
 			{
-				viewPort.prevItem = new ViewPortItem( columns, 0, 0 )
-				viewPort.currentItem = new ViewPortItem( columns, 0, 0 );
-				viewPort.nextItem = new ViewPortItem( columns, 1, 0 );
+				data.viewPort.currentItem = new ViewPortItem( data, 0, 0 );
+				data.viewPort.prevItem = new ViewPortItem( data, 0, 0 )
+				data.viewPort.nextItem = new ViewPortItem( data, 1, 1 );
 			},
 			// 3
 			setUpViewPort: function ( structure )
@@ -108,7 +108,7 @@
 				var prevArrowId = "dzPrevArrow";
 				var nextArrowId = "dzNextArrow";
 
-				var html = "<nav id='" + containerId + "'><a id ='" + prevArrowId + "' href='#' >prev</a><a id='" + nextArrowId + "' href='#'>next</a></nav>"
+				var html = "<nav id='" + containerId + "'><a id ='" + prevArrowId + "' href='#' style='display:none;'>prev</a><a id='" + nextArrowId + "' href='#'>next</a></nav>"
 				var $html = $( html );
 				structure.$dzieuo.append( $html );
 
@@ -140,10 +140,11 @@
 			setUpVerticalNav: function ( $dzieuo )
 			{
 				var containerId = "dzVerticalNav";
+				var className = "dz-nav-arrows"
 				var prevArrowId = "dzUpArrow";
 				var nextArrowId = "dzDownArrow";
 
-				var html = "<nav id='" + containerId + "' href='#' '><a id ='" + prevArrowId + "'>up</a><a id='" + nextArrowId + " href='#' '>down</a></nav>"
+				var html = "<nav id='" + containerId + "'><a id ='" + prevArrowId + "' class='" + className + "' href='#'>up</a><a id='" + nextArrowId + "' class='" + className + "' href='#'>down</a></nav>"
 				$dzieuo.append( html );
 			},
 			// 7
@@ -174,6 +175,16 @@
 					_plugin.beginHorizontalTransition( data, -( $( window ).width() - 20 ), data.viewPort.prevItem.column );
 				} )
 
+				data.structure.$dzieuo.on( "click", "#dzDownArrow", function ()
+				{
+					_plugin.beginVerticalTransition( data, data.viewPort.nextItem.get$Row() );
+				} )
+
+				data.structure.$dzieuo.on( "click", "#dzUpArrow", function ()
+				{
+					_plugin.beginVerticalTransition( data, data.viewPort.prevItem.get$Row() );
+				} )
+
 				data.structure.$dzieuo.on( "click", ".dz-paging-item", function ()
 				{
 					var targetColumn = $( this ).data( "column" );
@@ -193,14 +204,16 @@
 		///////////////////////////////////////////
 		//private plugin other methods
 		///////////////////////////////////////////
-		_plugin.beginHorizontalTransition = function ( data, offset, targetColumn )
+		_plugin.beginHorizontalTransition = function ( data, leftOffset, targetColumn )
 		{
+			var columnObj, $row, topOffset;
+
 			if ( data.viewPort.isAnimationInProgressX )
 			{
 				return false;
 			}
 
-			if ( offset > 0 )
+			if ( leftOffset > 0 )
 			{
 				if ( ( data.viewPort.currentItem.column + 1 ) === data.structure.numOfColumns )
 				{
@@ -223,26 +236,38 @@
 
 			function moveToNext( data, targetColumnIndex )
 			{
-				var $targetColumn = data.structure.columns[targetColumnIndex].$column;
+				var $row;
+				var columnObj = data.structure.columns[targetColumnIndex]
 				var left = data.structure.$viewPort.offset().left;
 
+				$row = columnObj.rows[columnObj.currentRow];
+
+				data.structure.$dzieuo.scrollTop( 0 );
+				data.structure.$dzieuo.scrollTop( $row.offset().top );
+
+				columnObj.$column.show();
+
 				data.viewPort.prevItem.column = data.viewPort.currentItem.column;
-				data.viewPort.nextItem.column = targetColumnIndex;
-				data.viewPort.currentItem.column = targetColumnIndex;
+				data.viewPort.nextItem.column = targetColumnIndex; 			
+			    data.viewPort.currentItem.column = targetColumnIndex;
 
 				toggleHorizontalArrowVisibility( targetColumnIndex, data.viewPort.prevItem.column, data.structure );
 
-				$targetColumn.show();
-				$targetColumn.css( { "left": offset - left, "top": 0 } );
+				columnObj.$column.css( { "left": leftOffset - left, "top": 0 } );
 
 				data.viewPort.isAnimationInProgressX = true;
 				data.structure.$viewPort.animate( {
-					"left": ( left - offset )
+					"left": ( left - leftOffset )
 				}, 600, function ()
 				{
+				
 					data.viewPort.prevItem.get$Column().hide();
 					data.viewPort.prevItem.column = targetColumnIndex - 1;
 					data.viewPort.nextItem.column++;
+
+					data.viewPort.currentItem.row = columnObj.currentRow;
+					data.viewPort.prevItem.row = columnObj.currentRow - 1;
+					data.viewPort.nextItem.row = columnObj.currentRow + 1;
 
 					data.viewPort.isAnimationInProgressX = false;
 				} );
@@ -250,27 +275,38 @@
 
 			function moveToPrevious( data, targetColumnIndex )
 			{
-				var $targetColumn = data.structure.columns[targetColumnIndex].$column;
+				var $row;
+				var columnObj= data.structure.columns[targetColumnIndex]
 				var left = data.structure.$viewPort.offset().left;
 
+				$row = columnObj.rows[columnObj.currentRow];
+
+				data.structure.$dzieuo.scrollTop( 0 );
+				data.structure.$dzieuo.scrollTop( $row.offset().top );
+
+				columnObj.$column.show();
+				
 				data.viewPort.nextItem.column = data.viewPort.currentItem.column;
 				data.viewPort.prevItem.column = targetColumnIndex;
 				data.viewPort.currentItem.column = targetColumnIndex;
 
 				toggleHorizontalArrowVisibility( targetColumnIndex, data.viewPort.nextItem.column, data.structure );
 
-				$targetColumn.show();
-				$targetColumn.css( { "left": offset - left, "top": 0 } );
+				columnObj.$column.css( { "left": leftOffset - left, "top": 0 } );
 
 				data.viewPort.isAnimationInProgressX = true;
 
 				data.structure.$viewPort.animate( {
-					"left": ( left - offset )
+					"left": ( left - leftOffset )
 				}, 600, function ()
 				{
 					data.viewPort.nextItem.get$Column().hide();
 					data.viewPort.nextItem.column = targetColumnIndex + 1;
 					data.viewPort.prevItem.column--;
+
+					data.viewPort.currentItem.row = columnObj.currentRow;
+					data.viewPort.nextItem.row = columnObj.currentRow + 1;
+					data.viewPort.prevItem.row = columnObj.currentRow - 1;
 
 					data.viewPort.isAnimationInProgressX = false;
 				} );
@@ -291,7 +327,7 @@
 				{
 					data.structure.$prevHorizontalArrow.fadeOut();
 
-					if ( currentIndex + 1 === structure.numOfColumns)
+					if ( currentIndex + 1 === structure.numOfColumns )
 					{
 						data.structure.$nextHorizontalArrow.fadeIn();
 					}
@@ -319,14 +355,66 @@
 			}
 		};
 
-		_plugin.beginVerticaltransition = function ( data, offset )
+		_plugin.beginVerticalTransition = function ( data, $row )
 		{
-			data.structure.$viewPort.animate( {
-				"top": offset
-			}, 600, function ()
+			var $dzieuo, oldScrollTop, viewPort = data.viewPort;
+			var moveDown = false;
+
+			var o = data.viewPort.nextItem;
+
+			if ( viewPort.isAnimationInProgressY )
 			{
-				//Animation complete			
+				return false;
+			}
+
+			if ( viewPort.currentItem.get$Row().offset().top < $row.offset().top )
+			{
+				if ( ( viewPort.currentItem.row + 1 ) === data.structure.columns[viewPort.currentItem.column].numOfRows )
+				{
+					return false;
+				}
+
+				moveDown = true;
+			} else
+			{
+				if ( viewPort.currentItem.row === 0 )
+				{
+					return false;
+				}
+			}
+
+			$dzieuo = data.structure.$dzieuo;
+			oldScrollTop = $dzieuo.scrollTop();
+
+			viewPort.isAnimationInProgressY = true;
+			$dzieuo.animate( { scrollTop: $dzieuo.scrollTop() + $row.offset().top }, 600, function ()
+			{
+				viewPort.isAnimationInProgressY = false;
+
+				if ( moveDown )
+				{
+					viewPort.prevItem.row = viewPort.currentItem.row;
+					viewPort.currentItem.row = viewPort.nextItem.row;
+
+					if (( viewPort.currentItem.row + 1 ) !== data.structure.columns[viewPort.currentItem.column].numOfRows )
+					{
+						viewPort.nextItem.row++;
+					}
+
+				} else
+				{
+					viewPort.nextItem.row = viewPort.currentItem.row;
+					viewPort.currentItem.row = viewPort.prevItem.row;
+					if ( ( viewPort.currentItem.row - 1 ) !== 0 )
+					{
+						viewPort.prevItem.row--;
+					}
+				}
+
+				data.structure.columns[viewPort.currentItem.column].currentRow = viewPort.currentItem.row;
+
 			} );
+
 		}
 
 
@@ -334,7 +422,7 @@
 		// initialize plugin
 		//////////////////////////////////////
 		_plugin.setUpDataStructure( _data.structure );
-		_plugin.setUpViewPortItems( _data.viewPort, _data.structure.columns );
+		_plugin.setUpViewPortItems( _data );
 		_plugin.setUpViewPort( _data.structure );
 		_plugin.setUpHorizontalNav( _data.structure );
 		_plugin.setUpHorizontalPaging( _data.structure );
